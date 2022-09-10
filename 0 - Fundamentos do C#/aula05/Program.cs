1430571baa0aa935d645da35527f406691f62419
Natan Cordeiro <o.natan1907@gmail.com>
2022-09-10 19:48:33 -0300

    métodos

---
+++ 0 - Fundamentos do C#/aula05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula05
+{
+    class Program 
+    { 
+        static void Main(string[] args) 
+        //       Método Main
+        {      
+            MeuMetodo();
+
+            string nome = RetornaNome("Natan", "Cordeiro");
+            Console.WriteLine(nome);
+        }
+    
+        static void MeuMetodo()
+        {// void = não retorna nada
+        
+            Console.WriteLine("C# é legal");
+        }
+    
+        static string RetornaNome(string nome, string sobrenome,int idade = 22) 
+        {//                       par. obrigatórios             Parâmetro opcional 
+            return nome + " " + sobrenome + " tem " + idade.ToString();
+        }
+
+    }
+}
