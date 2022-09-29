9b0d346180a9733678cfa8f13af80a0ca6fbc738
Natan Cordeiro <o.natan1907@gmail.com>
2022-09-29 20:16:31 -0300

    aula de guid (identificadores)

---
+++ 0 - Fundamentos do C#/aula07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula07 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            // Criando um ID aleatório 
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            // Passando um GUID novo (aceita somente esse formato!)
+            id = new Guid("4cad989f-f0df-4d26-8447-d66c6ab3a5b1");
+            
+            if(id == Guid.NewGuid()); 
+            Console.WriteLine(id.ToString().Substring(0, 8)); //pegando somente os 8 primeiros caracteres
+        }
+    }
+}
