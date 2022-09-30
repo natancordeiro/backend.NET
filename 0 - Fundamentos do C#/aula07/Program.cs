f0307b7cf6f2267f9a5ea4f13b09296e86765f8a
Natan Cordeiro <o.natan1907@gmail.com>
2022-09-29 22:51:48 -0300

    interpolação de strings

--- 0 - Fundamentos do C#/aula07/Program.cs
+++ 0 - Fundamentos do C#/aula07/Program.cs
@@ -15,6 +15,17 @@ namespace aula07
             
             if(id == Guid.NewGuid()); 
             Console.WriteLine(id.ToString().Substring(0, 8)); //pegando somente os 8 primeiros caracteres
+
+            //==================================================================
+            var price = 10.2;
+            // Interpolação de String
+
+            //var texto = "O preço do produto é " + price + " apenas na promoção";
+            //var texto = string.Format("O preço do produto é {0} apenas na promoção", price);
+            var texto = $@"O preço do produto é 
+            {price} apenas \n na promoção"; // @ para pular linhas (não quebra no \n)
+
+            Console.WriteLine(texto);
         }
     }
 }
